9a3dd73d87cae335429e43c1a7c5fc0361e68e3a
Maciej <wielgosz.mac@gmail.com>
2025-06-17 15:45:08 +0200

    Rebuild frontend without base path

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+/** 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/secure-login/",
@@ -10,6 +10,10 @@ export default defineConfig({
       "/api": "http://localhost:3001",
     },
   },
+});
+*/
+export default defineConfig({
+  plugins: [react()],
 });
 
 ////////////////////////////////////////////////
