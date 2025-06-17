352db6cd0f7ce8c5982e79ff2a55c9bec87c95dc
Maciej <wielgosz.mac@gmail.com>
2025-06-17 15:18:47 +0200

    Serve frontend from client/dist

--- server/index.js
+++ server/index.js
@@ -76,7 +76,9 @@ app.use(
   express.static(path.join(__dirname, "uploads"))
 );
 
-app.get("*", (req, res) => {
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
