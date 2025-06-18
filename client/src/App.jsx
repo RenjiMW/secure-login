6d150881c65103dc779672f93156032661f0404c
Maciej <wielgosz.mac@gmail.com>
2025-06-18 19:37:15 +0200

    Fix: add root redirect route to /login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import EditProfile from "./pages/EditProfile";
@@ -8,6 +13,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/edit" element={<EditProfile />} />
