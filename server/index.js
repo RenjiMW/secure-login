120620951482d8c919f00a4ba4020bdf7d1638f9
Maciej <wielgosz.mac@gmail.com>
2025-06-18 19:50:52 +0200

    Fix session cookie sameSite for cross-domain login

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      sameSite: "lax",
+      sameSite: "none",
       secure: true, // NODE_ENV === 'production'
     },
   })
