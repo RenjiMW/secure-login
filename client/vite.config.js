7a9f7f42d0f582fb8b56f5a725d67a69da78fe2c
Maciej <wielgosz.mac@gmail.com>
2025-07-29 19:40:27 +0200

    Prepare frontend for Render deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -27,7 +27,7 @@ export default defineConfig(({ mode }) => {
     define: {
       "import.meta.env.VITE_BACKEND_URL": JSON.stringify(
         isProduction
-          ? "https://secure-login-full.onrender.com"
+          ? "https://secure-login-backend-sfga.onrender.com"
           : "http://localhost:3001"
       ),
     },
