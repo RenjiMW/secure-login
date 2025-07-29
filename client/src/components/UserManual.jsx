d761cee064ce2caab30ee41727d90db09dbd574b
Maciej <wielgosz.mac@gmail.com>
2025-07-29 18:28:20 +0200

    Initial commit: start frontend/backend split

---
+++ client/src/components/UserManual.jsx
@@ -0,0 +1,19 @@
+function UserManual() {
+  return (
+    <div>
+      <p> This is just a demo project.</p>
+      <p>
+        To see details please visit my{" "}
+        <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>{" "}
+        page
+      </p>
+      <h4>Use a previously defined user to log in</h4>
+      <span>User name: skybug5</span>
+      <span>Password: mix852</span>
+    </div>
+  );
+}
+
+export default UserManual;
