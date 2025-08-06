8699839f629873f78b02efddc02ff594c6f43a34
Maciej <wielgosz.mac@gmail.com>
2025-08-06 18:57:05 +0200

    changes in CORS policy

--- server/index.js
+++ server/index.js
@@ -64,7 +64,7 @@ app.use(
 app.use(
   cors({
     origin: isProduction
-      ? "https://secure-login-backend-sfga.onrender.com"
+      ? "https://secure-login-frontend.onrender.com"
       : "http://localhost:5173",
     credentials: true,
   })
